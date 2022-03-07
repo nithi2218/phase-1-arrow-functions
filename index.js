088c9589d7dd7d876adeb004b254d1fecc7fb292
Nithyadevi <nithi2218@gmail.com>
2022-03-07 15:40:26 +1100

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function() {
+    let a=2000, b=100;
+    return a / b ;
+}
+
+
+const square = x  =>  {
+    return x * x;
+  }
+square(5);
+
+const add = (a, b) => {
+    return a+b;
+  }
+  add(5, 6);
